f73a297d6c289e524542e1b64b832f24611347a9
unknown <flaming.june123@gmail.com>
2024-08-04 19:59:33 +0900

    20240804_2000

--- JS/index.js
+++ JS/index.js
@@ -1,26 +1,14 @@
-const myTarget = document.querySelector('.fade'); // ...手順(2)の部分
+const mordalBtn = document.querySelector('#JS-mordal');
+const mordalClose = document.querySelector('#JS-close');
 
-// IntersectionObserverのオプション設定　...手順(3)の部分
-let myOptions = {
-  root: null,
-  rootMargin: '0px 0px',
-  threshold: '0'
-};
-  
-  // myObserverにIntersectionObserverのインスタンスを代入
-  const myObserver = new IntersectionObserver(myIntersect, myOptions);
-  // observe(対象); として対象を監視
-  myObserver.observe(myTarget);
+const mordalContent = document.querySelector('.p-mordal-content');
 
-// 条件を満たした実行される関数
-function myIntersect(entries, myObserver) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      myTarget.classList.remove('fade');
-      myTarget.classList.add('is-animation');
-    
-	  // 監視を解除(インビューに入った最初だけアニメーションさせる)
-	  myObserver.unobserve(myTarget);
-	}
-  });
-}
+mordalBtn.addEventListener('click',() => {
+  mordalContent.showModal();
+  document.body.classList.add('is-scrollLock')
+},false);
+
+
+mordalClose.addEventListener('click',() => {
+  mordalContent.close();
+},false);
