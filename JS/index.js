c4b07f0a17a2aef00ce630912d7da545625fad95
前進主義芝原28 <flaming.june123@gmail.com>
2024-10-21 08:27:42 +0900

    20241021_0827

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,16 @@
-const mordalBtn = document.querySelector('#JS-mordal');
-const mordalClose = document.querySelector('#JS-close');
-
-const mordalContent = document.querySelector('.p-mordal-content');
-
-mordalBtn.addEventListener('click',() => {
-  mordalContent.showModal();
-  document.body.classList.add('is-scrollLock')
-},false);
-
-
-mordalClose.addEventListener('click',() => {
-  mordalContent.close();
-},false);
+const options = {
+	type      : 'loop',      // ループを有効にする
+	   perPage   : 1,           // 1ページに表示するスライド数
+	   perMove   : 1,           // 1度に移動するスライド数
+	   autoplay  : 'slide',       // 自動再生を無効にする
+	   interval  : 3000,        // 次のスライドが表示されるまでの時間（ミリ秒）
+	   pauseOnHover: true,      // マウスホバー時に一時停止
+	   pagination: false,       // ページネーションを無効にする
+	   arrows    : false,        // 前後の矢印を表示
+	   drag      : false,       // ドラッグでの移動を無効にする
+	   gap       : 10,
+ };
+ 
+ const splide = new Splide(".splide", options);
+ 
+ splide.mount(window.splide.Extensions);
